fcc1afe329b74d90bc809931cd346bb31fb6ea6a
ShafinRME <shafin.rmedu@gmail.com>
2026-02-16 12:09:04 +0600

    Tour type from is under developing phase

--- src/components/modules/Admin/Division/AddDivisionModal.tsx
+++ src/components/modules/Admin/Division/AddDivisionModal.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import SingleImageUploader from "@/components/SingleImageUploader";
 import { Button } from "@/components/ui/button";
 import {
